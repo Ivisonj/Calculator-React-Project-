cc48a2a4bbb8d6620d44ce9553bc6e7389697a76
Ivisonj <ivison_j@outlook.com>
2023-02-20 17:05:49 -0300

    Implementação dos botões e display

---
+++ src/components/Buttons/Button.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import './Button.css';
+
+export default props => {
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+    
+    return (
+        <button 
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+            {props.label}
+        </button>
+    )
+}
+//exemplo 2 condições para aplicação de classe
+// export default props => 
+//     <button className={`
+//         button
+//         ${props.operation ? 'operation' : ''}
+//         ${props.double ? 'double' : ''}
+//         ${props.triple ? 'triple' : ''}
+//     `}>
+//         {props.label}
+//     </button>
