f090c143cf747f85112d4a0e63844642fb513faf
Ivisonj <ivison_j@outlook.com>
2023-02-21 13:58:55 -0300

    Implementação da lógica

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -4,37 +4,93 @@ import './Calculator.css';
 import Button from "../components/Buttons/Button";
 import Display from "../components/Display/Display";
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+}
+
 export default class Calculator extends Component {
 
+    state = {...initialState};
+
     constructor(props) {
-        super(props)
-        this.clearMemory = this.clearMemory.bind(this)
-        this.setOperation = this.setOperation.bind(this)
-        this.addDigit = this.addDigit.bind(this)
-    }
+        super(props);
+        this.clearMemory = this.clearMemory.bind(this);
+        this.setOperation = this.setOperation.bind(this);
+        this.addDigit = this.addDigit.bind(this);
+    };
 
     clearMemory() {
-        console.log('Limpar');
+        this.setState({...initialState});
     };
 
     setOperation(operation) {
-        console.log(operation)
+        if(this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true });
+        } else {
+            const equals = operation === '=';
+            const currentOperation = this.state.operation;
+
+            const values = [...this.state.values];
+
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`); //poderia usar if e else ou sweet
+                if(isNaN(values[0]) || !isFinite(values[0])) { //resolver isNaN
+                    this.clearMemory()
+                    return
+                }
+            } catch(e) {
+                values[0] = this.state.values[0];
+            };
+
+            values[1] = 0;
+
+            this.setState({
+                displayValue: values[0], 
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals, 
+                values
+            });
+        };
     };
 
     addDigit(n) {
         console.log(n)
+        if(n === '.' && this.state.displayValue.includes('.')) {
+            return
+         //evitar ter dois pontos    
+        };
+
+        const clearDisplay = this.state.displayValue === '0' //limpará o display se digito for 0 ou a var for treu do estado 
+            || this.state.clearDisplay; //evitar 0 a esquerda
+        const currentValue = clearDisplay ? '' : this.state.displayValue; //se precissar limpar o display o valor será vazio ou o valor atual do display
+        const displayValue =   currentValue + n; //novo valor do display
+        this.setState({ displayValue, clearDisplay: false }); 
+
+        if (n !== '.') {
+            const i = this.state.current;
+            const newValue = parseFloat(displayValue);
+            const values = [...this.state.values];
+            values[i] = newValue;
+            this.setState({ values });
+            console.log(values);
+        };
     };
 
     render() {
         return (
             <div className="calculator">
-                <Display value={100} />
+                <Display value={this.state.displayValue} />
                 <Button label="AC" click={this.clearMemory} triple/>
                 <Button label="/"  click={this.setOperation} operation/>
                 <Button label="7"  click={this.addDigit}/>
                 <Button label="8"  click={this.addDigit}/>
                 <Button label="9"  click={this.addDigit}/>
-                <Button label="x"  click={this.setOperation} operation/>
+                <Button label="*"  click={this.setOperation} operation/>
                 <Button label="4"  click={this.addDigit}/>
                 <Button label="5"  click={this.addDigit}/>
                 <Button label="6"  click={this.addDigit}/>
